dbf95d39de689e35dfd5690498332c347bf96990
Daniel Arteaga <darteaga@us.es>
2016-06-15 14:35:34 +0200

    added logger

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ var jsyaml = require('js-yaml');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var logger = require('winston');
 
 var serverPort = (process.env.PORT || 3001);
 var app = express();
+logger.default.transports.console.timestamp = true;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use((req, res, next) => {
+		var requestInfo = req.method + ' => ' + req.headers['host'] + req.url + ', params: '
+		+ JSON.stringify(req.query) +  ', with:  ' + JSON.stringify(req.body);
+		logger.info(requestInfo);
+		next();
+});
 // swaggerRouter configuration
 var options = {
 	swaggerUi: '/swagger/v1.json',
